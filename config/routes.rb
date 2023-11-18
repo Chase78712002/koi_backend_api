a39671862096a6ef6a831034efe96834a32bf2b0
Alvin Lin <alvinch.lin@gmail.com>
2023-11-18 14:16:03 -0800

    Mount Lookbook in routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,9 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "home#index"
   get "home", to: "home#index"
+
+  if Rails.env.development?
+    mount Lookbook::Engine, at: "/lookbook"
+  end
+
 end
