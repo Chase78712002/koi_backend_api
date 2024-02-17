7f8c8a4b7ad4d27260329d45d3a33c041d33e908
Alvin Lin <alvinch.lin@gmail.com>
2024-02-16 22:23:27 -0800

    Restart everything - set up a home page - set up an about page

--- app/views/teams/_team.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! team, :id, :name, :registered_on, :captain, :paid, :created_at, :updated_at
-json.url team_url(team, format: :json)
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  resources :teams
+  get 'about', to: 'pages#about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   
   # Defines the root path route ("/")
-  root "home#index"
-  get "home", to: "home#index"
+  root "pages#home"
 
   if Rails.env.development?
     mount Lookbook::Engine, at: "/lookbook"
